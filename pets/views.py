f734cba62a6f756b6a2a4e1c8ac9b5b304a525f5
Ja11ly <johnmaina2511@gmail.com>
2025-08-16 18:39:51 +0300

    Add Pet serializer, viewset, and API endpoints

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Pet
+from .serializers import PetSerializer
 
-# Create your views here.
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+    # Only admin can create/update/delete pets
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
