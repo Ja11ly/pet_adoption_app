96b1f0d9f11a7d103634850477cdd572966fdaea
Ja11ly <johnmaina2511@gmail.com>
2025-08-24 12:46:36 +0300

    feat: add filtering and search for pets

--- pets/views.py
+++ pets/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters 
 from .models import Pet
 from .serializers import PetSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['breed', 'location', 'availability']
+    search_fields = ['name', 'species', 'breed']
 
     # Only admin can create/update/delete pets
     def get_permissions(self):
