33acb5a069163b823dff6c2c427026a48e0dc6b0
Ja11ly <johnmaina2511@gmail.com>
2025-08-27 20:30:37 +0300

    have added filtering and ordering for Adoptions and Reviews

--- adoptions/views.py
+++ adoptions/views.py
@@ -4,10 +4,17 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import AdoptionRequest
 from .serializers import AdoptionRequestSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class AdoptionRequestViewSet(viewsets.ModelViewSet):
     serializer_class = AdoptionRequestSerializer
     permission_classes = [permissions.IsAuthenticated]
+    queryset = AdoptionRequest.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["status", "user", "pet"]
+    ordering_fields = ["request_date"]
+    ordering = ["-request_date"]
 
     def get_queryset(self):
         # Admins see all, users see their own
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Review
+from .serializers import ReviewSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ReviewSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     queryset = Review.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["rating", "pet", "user"]
+    ordering_fields = ["created_at", "rating"]
+    ordering = ["-created_at"]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
