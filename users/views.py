fdc8b0320b38bf46d1aee8de3bd15549508d3ccd
Ja11ly <johnmaina2511@gmail.com>
2025-09-02 23:16:13 +0300

    removed users/serializers.py

--- users/views.py
+++ users/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .serializers import UserSerializer
+#from .serializers import UserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework import serializers
-from .serializers import RegisterSerializer
+#from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
 from .permissions import IsOwnerOrAdminOrCreateOnly
+from rest_framework import serializers
+from django.contrib.auth.models import User
 
+# For general user details
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
-User = get_user_model()
+#User = get_user_model()
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,6 +27,7 @@ class UserViewSet(viewsets.ModelViewSet):
     
 
 class RegisterSerializer(serializers.ModelSerializer):
+    
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -32,6 +40,8 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             password=validated_data['password']
         )
+        user.is_active = True   
+        user.save()
         return user
 
 class RegisterView(generics.CreateAPIView):
